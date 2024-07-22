34f3295b34694ffa6d2b77b3305fddae828db56f
Etinofficial01 <emmanuelosemwengie@gmail.com>
2024-07-22 14:53:06 +0100

    made changes to JavaScript, CSS and added a background image

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
+//create variables to store API key and API URL
 const apiKey = "2d2b5f440c61be41bf32d9ed601b4531";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+//create variables for input box and button
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 
+//create variable for weather icon
 const weatherIcon = document.querySelector(".weather-icon")
 
+
+//write a function, passing in the city object, that checks for blank input, error and city name
 async function checkWeather(city){
+    if(searchBox.value === ""){
+        alert("You must write something!");
+        return;
+    }
+
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
     if(response.status == 404){
+        //display error meaasge and hide weather info 
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    }else{
+    }
+    else{
         var data = await response.json();
 
 
@@ -21,7 +33,8 @@ async function checkWeather(city){
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-
+        
+        //changing weather icon image
         if(data.weather[0].main == "Clouds"){
             weatherIcon.src = "images/clouds.png";
         }else if(data.weather[0].main == "Rain"){
@@ -33,18 +46,19 @@ async function checkWeather(city){
         }else if(data.weather[0].main == "Mist"){
             weatherIcon.src = "images/mist.png";
         }
-
+        
+        //display weather info and hide error meaasge
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
+        
     }
 
-
-
+    searchBox.value = "";
 }
 
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
-})
+});
 
   
         
